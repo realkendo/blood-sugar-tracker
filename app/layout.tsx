d78e86a548fe44a7f79b8dccb359843421fac7b6
realkendo <kennethistifanus1@gmail.com>
2025-03-30 22:54:44 +0100

    title changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Blood Sugar Tracker",
+  description: "NextJS web application for health tracking",
+  generator: "K3nd0",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
