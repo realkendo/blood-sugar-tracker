55d5a14f086c6a37ce97c4e0ac0879fdc0e08a15
realkendo <kennethistifanus1@gmail.com>
2025-03-31 16:56:39 +0100

    omo

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,14 +1,47 @@
-"use client"
+// "use client"
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+// import * as React from "react"
+// import * as ProgressPrimitive from "@radix-ui/react-progress"
 
-import { cn } from "@/lib/utils"
+// import { cn } from "@/lib/utils"
+
+// const Progress = React.forwardRef<
+//   React.ElementRef<typeof ProgressPrimitive.Root>,
+//   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
+// >(({ className, value, ...props }, ref) => (
+//   <ProgressPrimitive.Root
+//     ref={ref}
+//     className={cn(
+//       "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
+//       className
+//     )}
+//     {...props}
+//   >
+//     <ProgressPrimitive.Indicator
+//       className="h-full w-full flex-1 bg-primary transition-all"
+//       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+//     />
+//   </ProgressPrimitive.Root>
+// ))
+// Progress.displayName = ProgressPrimitive.Root.displayName
+
+// export { Progress }
+
+"use client";
+
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { cn } from "@/lib/utils";
+
+interface ProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorClassName?: string; // Add this line
+}
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  ProgressProps // Use the extended props interface
+>(({ className, value, indicatorClassName, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -18,11 +51,16 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className={cn(
+        // Use `cn` to merge classes
+        "h-full w-full flex-1 bg-primary transition-all",
+        indicatorClassName // Apply the custom class
+      )}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
-))
-Progress.displayName = ProgressPrimitive.Root.displayName
+));
 
-export { Progress }
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
